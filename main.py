0220bccf42d27260552faba4f9ee4c61ecfcff35
Daniela Molina <admolinaib@cesde.net>
2024-11-05 20:55:49 -0500

    dmi feat se configura el cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.api.models.SQLtables import Base
 from app.api.routes.endpoints import rutas
 
 from starlette.responses import RedirectResponse
+from starlette.middleware.cors import CORSMiddleware
 
 #metadata de las tablas es toda la info de las tablas. 
 #Crear las tablas de sql desde python
@@ -11,6 +12,15 @@ Base.metadata.create_all(bind=engine)
 
 #Variable para administrar la aplicacion
 app = FastAPI()
+
+#Configurar el protocolo CORS 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"], #para que sea publico y pueda conectarse cualquiera desde cualquier origen, es decir, cualquier lugar
+    allow_credentials = True,
+    allow_methods = ["*"], #get, put, etc
+    allow_headers = ["*"]
+)
 
 #Activar el api
 @app.get("/")
